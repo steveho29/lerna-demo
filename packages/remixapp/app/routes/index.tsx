8bb632a4f5361e451ed3c85c33e347f84ec91050
steveho29 <hongocminhduc29@gmail.com>
2022-12-21 15:11:43 +0000

    update package name

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,5 +1,5 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import {Header} from '@demo/header';
+import {Footer} from '@demo/footer';
 
 export default function Index() {
     return (
